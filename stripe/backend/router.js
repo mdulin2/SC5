0f5facaf706f651c05972d241516c18d5ea998d2
Ubuntu <mdulin2@zagmail.gonzaga.edu>
2023-08-17 18:05:04 +0000

    Initial POC of stripe challenges backend

--- stripe/backend/router.js
+++ stripe/backend/router.js
@@ -23,8 +23,22 @@ let product_id = "";
 let price_id = "";
 let ip = ""; 
 
+// http://maxwelldulin.com:5000/user/Maxwell
+app.get("/winner/:userid", async function (req, res) {
+  var d = await db.get(`SELECT * FROM Users WHERE UserID = ?`, req.params.userid)
+  if(d === undefined){ // Create the user if they don't exist.
+      res.send({"error" : "User DNE"});
+      return; 
+  }
+  if(d.Amount >= 20){
+      res.send({"flag" : "SC5{asdfasfd}"})
+      return;
+  }
+  res.send({"error" : "Need to get 20 orders to get flag"})
+   
+});
 
-// curl -X POST http://127.0.0.1:5000/webhook --data '{"type" : "payment_intent.succeeded"}' -H 'Content-Type: application/json'
+// curl -X POST http://127.0.0.1:5000/webhookssion.completed", "data" :{"object" : {"metadata" : {"payment_id" : 4}}}}' -H 'Content-Type: application/json'
 app.post('/webhook', express.json({type: 'application/json'}), async function (req, res) {
 
     const event = req.body; 
@@ -40,7 +54,7 @@ app.post('/webhook', express.json({type: 'application/json'}), async function (r
     // TODO - Check data in the 'Owners' to see if the ID for our order exists or not.
     // Handle the event
     switch (event.type) {
-      case 'payment_intent.succeeded':
+      case 'checkout.session.completed':
 
         if(!event.data){
           result = {"type" : "Missing 'data' in body"}
@@ -51,10 +65,24 @@ app.post('/webhook', express.json({type: 'application/json'}), async function (r
           result = {"type" : "Missing 'data.object' in body"}
           break; 
         }
+        var session_obj = event.data.object;
+        if(!('metadata') in event.data.object){
+          result = {"type" : "Missing 'data.object.metadata' in body"}
+          break;
+        }
 
-        const paymentIntent = event.data.object;
-        // TODO: Add to DB or something...
-        console.log(paymentIntent); 
+        if(!('payment_id') in event.data.object.metadata){
+          result = {"type" : "Missing 'data.object.metadata.payment_id' in body"}
+          break;
+        }
+
+        var tracking_id = event.data.object.metadata.payment_id;
+        var d = await db.get(`SELECT * FROM Orders WHERE ID = ?`, [tracking_id]);
+		 console.log(d)
+
+   	var user = await db.get(`SELECT * FROM Users WHERE UserID = ?`, d.UserID)
+        updateUser(user.UserID, user.Amount + 1)
+	result = {"error" : false, "message" : "Successfully bought!"}
         break;
 
       default:
@@ -65,30 +93,68 @@ app.post('/webhook', express.json({type: 'application/json'}), async function (r
     res.json(result);
 });
 
-// curl -X POST http://127.0.0.1:5000/stripe
-app.post('/stripe', async function (req, res) {
-  console.log("Price Id:", price_id); 
+// curl -X POST http://127.0.0.1:5000/stripe/admin
+app.post('/stripe/:userid', async function (req, res) {
+
+   var user = await db.get(`SELECT * FROM Users WHERE UserID = ?`, req.params.userid)
+   console.log(d) 
+   if(user === undefined){
+	res.send({"error" : "user dne"})
+	return;
+   }
+   if(user.Amount > 2){
+	res.send({"error" : "User hit the limit"})
+	return;
+   }
+
+   // Get the amount of orders
+   var d = await db.get(`SELECT COUNT(ID) from Orders`);
+   var id;
+   if(d['COUNT(ID)'] == null){
+      id = 1;
+   }
+   else {
+      id = d['COUNT(ID)'] + 1;
+   }
+   console.log(d, id)
+
+ console.log("Price Id:", price_id);
 
   const session = await stripe.checkout.sessions.create({
-    
     success_url: 'https://maxwelldulin.com/success',
     line_items: [
       {price: price_id, quantity: 1},
     ],
     mode: 'payment',
+    metadata : {
+	payment_id: id
+    }
   });
 
-  console.log(session.url); 
+  createOrder(user.UserID, session.url);
 
   // Track this info in the 'Orders' table.
-  res.send({"redirect_url" :session.url});
-
+  res.send({"redirect_url" :session.url, "id" : id});
+ 
   // Where to redirect the page to - Stripe!
   return session.url; 
 });
 
+// curl http://maxwelldulin.com:5000/user/admin
+app.get('/user/:userid', async function (req, res) {		
+
+  var d = await db.get(`SELECT * FROM Users WHERE UserID = ?`, req.params.userid)
+  res.send(d)
+});
+
+app.get('/Orders/:userid', async function (req, res) {		
+
+  var d = await db.get(`SELECT * FROM Orders WHERE UserID = ?`, req.params.userid)
+  res.send(d)
+});
 
 // Not the vuln :) Just trying to make it easier to use!
+// curl -X POST http://127.0.0.1:5000/login --data '{"user" : "admin"'} -H "Content-Type:application/json
 app.post('/login',  express.json({type: 'application/json'}), async function (req, res) {
   var data = req.body; 
   if(!req.body || !('user' in req.body)){
@@ -97,10 +163,10 @@ app.post('/login',  express.json({type: 'application/json'}), async function (re
   }
 
   var id; 
-  var d = await db.get(`SELECT * FROM Users WHERE Name = ?`, data.user);
+  var d = await db.get(`SELECT * FROM Users WHERE UserID = ?`, data.user);
   if(d === undefined){ // Create the user if they don't exist.
     id = await createUser(data.user, 0); 
-    d = await db.get(`SELECT * FROM Users WHERE Name = ?`, data.user);
+    d = await db.get(`SELECT * FROM Users WHERE UserID = ?`, data.user);
     id = d['UserID'];
   }
   else{
@@ -111,8 +177,6 @@ app.post('/login',  express.json({type: 'application/json'}), async function (re
   res.send({"error" : false, "ID" : id});
   return; 
 }); 
-
-
 
 async function createHitman(){
   const products = (await stripe.products.list({
@@ -157,12 +221,21 @@ async function createPrice(){
 }
 
 async function setupWebhook(){
+
+  const webhookEndpoints = await stripe.webhookEndpoints.list({
+  limit: 3,
+});
+
+  console.log(webhookEndpoints)
+  if(webhookEndpoints.data.length > 1){
+     return; 
+  }
+
   // TODO - get IP address/domain dynamically here
   const endpoint = await stripe.webhookEndpoints.create({
-    url: ip + ":5000/webhook",
+    url: "http://" + "maxwelldulin.com" + ":5000/webhook",
     enabled_events: [
-      'payment_intent.payment_failed',
-      'payment_intent.succeeded',
+      'checkout.session.completed'
     ],
   });
 }
@@ -186,21 +259,28 @@ async function getRemoteIp(){
 async function initDb(){
 
   // Individual user tracking
-  query2 = "CREATE TABLE Users (UserID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR, Amount Integer) "; 
+  query2 = "CREATE TABLE Users (UserID VARCHAR PRIMARY KEY, Amount Integer) "; 
   await db.exec(query2);
 
   // Create session management for orders
-  var query = "CREATE TABLE Orders(ID INTEGER PRIMARY KEY AUTOINCREMENT, UserID INTEGER, Amount Integer, Session TEXT, FOREIGN KEY(UserID) REFERENCES Users(UserID)) "; 
+  var query = "CREATE TABLE Orders(ID INTEGER PRIMARY KEY AUTOINCREMENT, UserID VARCHAR, Amount Integer, Session TEXT, FOREIGN KEY(UserID) REFERENCES Users(UserID)) "; 
   await db.exec(query); 
-
 }
 
 async function createUser(name, amount){
-  return (await db.run("INSERT INTO Users(Name,Amount) VALUES(?,?)", [name, amount]));
+  return (await db.run("INSERT INTO Users(UserID,Amount) VALUES(?,?)", [name, amount]));
+}
+
+async function updateUser(id, amount){
+  return (await db.run("UPDATE Users SET Amount = ? WHERE UserID = ?", [amount, id]));
+}
+
+async function createOrder(UserId, Session){
+  return (await db.run("INSERT INTO Orders(UserID, Amount, Session) VALUES (?,1,?)", [UserId, Session]));
 }
 
 async function testData(){
-  await createUser("Maxwell", 1000); 
+  await createUser("Maxwell", 1)
 }
 
 async function init(){
@@ -216,7 +296,7 @@ async function init(){
   catch(err){
     console.log("DB already created..."); 
   }
-  // await setupWebhook();
+  await setupWebhook();
 }
 
 
@@ -225,7 +305,10 @@ stripe.prices.update(  'price_1NfsKxE8bmyyMPHw8sJC7vQk'
 ,{
   active: false
 })
+
 */
+
+//stripe.webhookEndpoints.del('we_1Ng9KXE8bmyyMPHwI5h9jfZ9')
 init();
 
 app.listen(5000);
--- stripe/docker_run.sh
+++ stripe/docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 sudo docker build . --tag stripe || exit 
-sudo docker run --privileged -d -p 5000:5000 -p 3000:3000 stripe || exit 
+sudo docker run --privileged -d -p 5000:5000 -p 3001:3001 stripe || exit 
 
 # DEBUG version -- goes into the container automatically
 docker_ps=$(sudo docker ps -q | head -n1) 
